bcda2a79a693c79652b0788bea6ee5ef268479a4
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2021-09-02 23:11:06 +0900

    implement snn

---
+++ loss/snn.py
@@ -0,0 +1,59 @@
+import torch
+
+class SNN(torch.nn.Module):
+    """
+    Implementation of Soft nearest neighbour loss [1].
+
+    [1] Nicholas Frosst, Nicolas Papernot, Geoffrey Hinton.
+        Analyzing and Improving Representations with the Soft Nearest Neighbor Loss.
+        pages: 2012–2020,
+        In ICML,
+        2019.
+        https://arxiv.org/abs/1902.01889
+    """
+    def __init__(self, device: torch.device, t: float = 1.0, reduce: str = "mean") -> None:
+
+        assert t > 0.
+        assert reduce in {"mean", "sum", "none"}
+
+        self._device = device
+        self._t = t
+        self._reduce = reduce
+
+        super().__init__()
+
+    def forward(self, features: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        N = len(features)
+        features = torch.nn.functional.normalize(features, p=2, dim=1)
+        sim = torch.matmul(features, features.t())
+        mask = ~torch.eye(N, dtype=torch.bool).to(self._device)  # remove self-similarity
+        sim = torch.exp(sim[mask].view(N, N-1) / self._t)
+        denom = torch.sum(sim, dim=1)
+
+        numerator_mask = (targets == targets.view(N, 1))[mask].view(N, N-1)
+        loss_per_sample = []
+        for s, n, d in zip(sim, numerator_mask, denom):
+            scores = s[n]
+            if len(scores):
+                loss_per_sample.append(torch.log(torch.sum(scores)/d))
+
+        loss = torch.stack(loss_per_sample)
+
+
+        if self._reduce != "none":
+            loss = torch.sum(loss)
+
+        if self._reduce == "mean":
+            loss /= N
+
+        return -loss
+
+
+if __name__ == "__main__":
+    device = torch.device("cpu")
+    features = torch.rand(4, 8).to(device)
+    targets = torch.tensor([0, 0, 0, 2]).to(device)
+
+    snn = SNN(device)
+
+    print(snn(features, targets))
